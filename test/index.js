02557274b49e868c7c177b1a88fe5b3e436bd0af
Anthony Graddy <anthony@locationq.com>
2017-10-05 20:51:16 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
-process.chdir('test');
+var path = require('path');
+process.chdir(path.dirname(__filename));
 var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+tap.assert.deepEqual(mod('./fixtures/crlf.txt'), ['one', 'two', 'three'], 'Should be equal.');
+
+tap.assert.deepEqual(mod('./fixtures/lf.txt'), ['one', 'two', 'three'], 'Should be equal.');
+
+tap.assert.deepEqual(mod('one\r\ntwo\r\nthree'), ['one', 'two', 'three'], 'Should be equal.');
+
+tap.assert.deepEqual(mod('one\ntwo\nthree'), ['one', 'two', 'three'], 'Should be equal.');
 
 
